89d39f2d658aad701527cb250800edd556356626
nandz080 <nandz080@gmail.com>
2023-02-17 05:59:57 -0800

    Task 1 create a program will assign a random number to the variable n each time it is executed

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,9 +1,13 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Description: 'assign a random number to the variable n each  execution'
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n, digit;
@@ -13,17 +17,11 @@ int main(void)
 	digit = n % 10;
 	printf("Last digit of %d ", n);
 	if (digit > 5)
-	{
 		printf("is %d and is greater than 5\n", digit);
-	}
-	if (digit == 0) 
-	{
-        	printf("is %d and is 0\n", digit);
-        }
-	if (0 > digit < 6) 
-	{
-        	printf("is %d and is less than 6 and not 0\n", digit);
-        }
-	/* your code goes there */
+	if (digit == 0)
+		printf("is %d and is 0\n", digit);
+	if (digit < 6 && digit < 0)
+		printf("is %d and is less than 6 and not 0\n", digit);
+/* your code goes there */
 	return (0);
 }
