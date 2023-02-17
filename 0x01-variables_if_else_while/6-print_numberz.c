442fbcae1ac3c076e71a38d09f972a5e39c7b168
nandz080 <nandz080@gmail.com>
2023-02-17 11:08:39 -0800

    Task 6 Write a program that prints all single digit numbers of base 10 starting from 0, followed by a new line

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'prints the alphabet in lowercase, followed by a new line'
+ *
+ * Return: Always 0 (Success)
+**/
+
+int main(void)
+{
+	char ch = '1';
+
+	for (ch = '1' ; ch <= '9'; ch++)
+	putchar(ch);
+	putchar('\n');
+	return (0);
+}
