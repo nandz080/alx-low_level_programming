b7bc6285d42ad933a8c21931e2d63904e534df70
nandz080 <nandz080@gmail.com>
2023-02-21 14:48:10 -0800

    Task 0 Write a program that prints _putchar

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,24 @@
+#ifndef main_h
+#define main_h
+/**
+ * file name: main.h
+ * Author: Nandi M
+ * Description: prototype function header file
+ */
+
+void print_alphabet(void);
+void jack_bauer(void);
+void print_alphabet_x10(void);
+void times_table();
+void print_to_98(int);
+void print_times_table(int);
+int _putchar(char);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int add(int n, int m);
+int print_last_digit(int n);
+
+
+#endif
