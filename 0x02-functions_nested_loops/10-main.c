88e097e7f25c341381945ada41722233255d43e8
nandz080 <nandz080@gmail.com>
2023-02-21 18:53:36 -0800

    Task 10 Write a function that adds two integers and returns the result

---
+++ 0x02-functions_nested_loops/10-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int add(int, int);
+
+int main(void)
+{
+    int n;
+
+    n = add(89, 9);
+    printf("%d\n", n);
+    return (0);
+}
