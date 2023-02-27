1c4fe2a361a68a012cebc44c30e20b95e8a5b28e
nandz080 <nandz080@gmail.com>
2023-02-27 15:19:42 -0800

    Task 3 Write a function that prints a string, followed by a new line, to stdout

---
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints a string followed bynew line to stdout.
+ *
+ * Return: Always 0.
+*/
+
+int main(void)
+{
+	char *str;
+
+	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+	_puts(str);
+	return (0);
+}
