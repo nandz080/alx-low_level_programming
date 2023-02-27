c21bfd9b271745af4f0f6f0f2c8c26758c158fc6
nandz080 <nandz080@gmail.com>
2023-02-27 15:56:35 -0800

    Task 7 Write a function that prints half of a string, followed by a new line

---
+++ 0x05-pointers_arrays_strings/7-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - function prints half of a string.
+ *
+ * Return: Always 0.
+*/
+
+int main(void)
+{
+	char *str;
+
+	str = "0123456789";
+	puts_half(str);
+
+	return (0);
+}
