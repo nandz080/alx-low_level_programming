91a5217307bf1101d7a6ff2bcf7c8d91ac68790b
nandz080 <nandz080@gmail.com>
2023-04-10 12:18:46 -0700

    TASK 2.1 Write a function that returns the value of a bit at a given index

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -11,5 +11,6 @@ int check_valid_string(const char *b);
 void print_binary(unsigned long int n);
 int _putchar(char c);
 void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
 
 #endif
