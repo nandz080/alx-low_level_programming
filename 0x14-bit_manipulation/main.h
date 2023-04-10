9509d3d0c616018e96a2e6b8e8bdf2f841bbfd62
nandz080 <nandz080@gmail.com>
2023-04-10 12:00:49 -0700

    task 1.1 Write a function that prints the binary representation of a number

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -8,5 +8,8 @@
 
 unsigned int binary_to_uint(const char *b);
 int check_valid_string(const char *b);
+void print_binary(unsigned long int n);
+int _putchar(char c);
+void print_binary(unsigned long int n);
 
 #endif
