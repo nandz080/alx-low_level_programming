a41cbad23f9b726eafccf2da23e395048eb87c95
nandz080 <nandz080@gmail.com>
2023-04-10 12:48:47 -0700

    TASK 6 Write a function that checks the endianness.

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * get_endianness - function that checks endianness
+ *
+ * Return: 0 if big endian, 1 if little endian
+ */
+
+int get_endianness(void)
+{
+	unsigned int test = 1;
+	char *endian = (char *)&test;
+
+	if (*endian)
+		return (1);
+	return (0);
+
+}
